b4d383d8a046ad3b70afe6dfeec80b6e2a8ec8c7
SantiagoMarini <santiagomarini1005@gmail.com>
2026-04-18 19:54:38 -0300

    C3 - Ejercicio 1 - Variables y operaciones

---
+++ c03-javascript/ej1-variables/app.js
@@ -0,0 +1,17 @@
+// Variables y operaciones
+
+const nombre = "Lucas";
+const edad = 20;
+const materia = "Programacion Web";
+
+// muestro el mensaje
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+// contador que empieza en 0 y le sumo 1 tres veces
+let contador = 0;
+contador = contador + 1;
+contador = contador + 1;
+contador = contador + 1;
+
+// muestro el valor final
+console.log(contador);
